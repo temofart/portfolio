3d620694080ba1482609d066a5d3aec6030e26fa
temofart <18art12@gmail.com>
2016-08-02 16:25:00 +0300

    fixed scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-$('a[href^="#"]').click(function(){
-var el = $(this).attr('href');
-$('body').animate({
-scrollTop: $(el).offset().top}, 1000);
-return false;
-});
+    $("a").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination - 0}, 800);
+        return false;
+    });
 });
